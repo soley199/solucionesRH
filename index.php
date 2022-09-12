720e3ffc167813333b2008012e3b2f922b77bd17
Marco Enciso <cd.marco.0106@gmail.com>
2022-09-12 15:56:17 -0500

    abramTermindada

---
+++ index.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="./libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href="./css/style.css" rel="stylesheet" >
+
+    <title>Hello, world!</title>
+  </head>
+  <body>}
+  
+  <nav class="navbar navbar-expand-lg navbar-light bg-transparent p-4">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="./img/ot.png" alt="" width="75" height="50" class="d-inline-block align-text-top">
+      </a>
+      <button class="navbar-toggler bg-success" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">
+            <span class="text-success fs-5 fw-bold">Soluciones Capital Humano</span></a>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="menu">
+        <li class="nav-item">
+          <a class="nav-link active"  href="#" text-success>Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Quienes Somos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Galeria</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" href="#seccion-contacto">Contacto</a>
+        </li>
+      </ul>
+      </form>
+    </div>
+  </div>
+</nav>
+<!-- Slider Imagenes -->
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/carusel/img1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5></h5>
+        <p></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/carusel/im2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5></h5>
+        <p></p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<section class="w-50 mx-auto text-center pt-5" id="intro">
+  <h1 class="text-white p-3 fs-2 border-top border-3">Empresa especializada en Reclutamiento y Selección de personala a <span class="text-primary">NIVEL NACIONAL</span></h1>
+  <p class="text-white p-3 pb-0 fs-4"><span class="text-primary">ENFOCADOS EN</span> Autobuses, Rabón, Torthon, Tractocamión sencillo y full</p>
+</section>
+
+
+
+
+
+<div class="container ">
+    <h2 class="pb-2 border-bottom text-white">Nuestros Clientes</h2>    
+  </div>
+
+  <section class="container-fluid">
+      <div class="row w-75 mx-auto servicio-fila">
+        <div class="col-lg-6 col-md-12 col-sm-12 d-flex  icono-wrap">
+          <img src="./img/EASO.png" class="img-fluid" alt="EASO" width="180" height="160">
+          <div class="text-white">
+          <h3 class="fs-5 mt-4 px-4 pb-1 ">TITULO</h3>
+            <p class="px-4">Proident velit sint duis minim. </p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 d-flex icono-wrap">
+          <img src="./img/UTEP.png" class="img-fluid" alt="EASO" width="180" height="160">
+          <div class="text-white">
+          <h3 class="fs-5 mt-4 px-4 pb-1">TITULO</h3>
+            <p class="px-4">Proident velit sint duis minim. </p>
+          </div>
+        </div>
+      </div>
+      <div class="row w-75 mx-auto servicio-fila">
+        <div class="col-lg-6 col-md-12 col-sm-12 d-flex icono-wrap">
+          <img src="./img/MYM.png"  class="img-fluid" alt="EASO" width="180" height="160">
+          <div class="text-white">
+            <h3 class="fs-5 mt-4 px-4 pb-1">TITULO</h3>
+            <p class="px-4">Proident velit sint duis minim. </p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 d-flex  icono-wrap">
+          <img src="./img/TUMSA.png" class="img-fluid" alt="EASO" width="180" height="160">
+          <div class="text-white">
+          <h3 class="fs-5 mt-4 px-4 pb-1">TITULO</h3>
+            <p class="px-4">Proident velit sint duis minim. </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+<section>
+  <div class="w-75 m-auto text-center" id="clientes">
+    <h1 class="mb-3 fs-2 text-white">Un equipo
+      <span class="text-primary">con resultado grandes</span></h1>
+
+      <p class="fs-4 text-white">Se realiza envió de candidatos
+        presentandolos a la empresa con un
+        formato de entrevista profunda y
+        trayectoria laboral 
+      </p>
+      <p class="fs-4 text-white">Se extiende garantia de permanencia por
+      30 días, inciando el dia de la contratación
+      del candidato.</p>
+  </div>
+  <div class="mt-5 text-center">
+    <img class="img-fluid" src="./img/equipo.png" alt="">
+  </div>
+</section>
+
+<div id="local" class="border-top border-2" >
+<div class="mapa">
+  <div class="gmap_canvas">
+    <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=95%20Cda.%20L%C3%A1zaro%20cardenas%20Tizayuca,%20Estado%20de%20Hidalgo&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      <a href="https://123movies-to.org">123movies</a>
+      <br>
+      <!--<style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style>-->
+      <a href="https://www.embedgooglemap.net">google map on web site</a>
+      <style>.gmap_canvas {overflow:hidden;background:none!important;height:100%;width:500px;}</style>
+    </div>
+  </div>
+  <div class="wrapper">
+    <h2 class="text-primary">Nuestra Ubicacion</h2>
+    <h2></h2>
+    <p class="fs-5 text-white">Incididunt aliquip id Lorem excepteur sit. Irure deserunt ea quis consequat 
+      dolor eiusmod duis quis adipisicing. Non tempor ipsum adipisicing veniam ullamco 
+      amet anim occaecat nisi dolore minim fugiat.</p>
+    <section class="d-flex " id="numeros-local">
+      <div>
+        <p class=" text-primary fs-5">248</p>
+        <p class="text-white">Días del año</p>
+      </div>
+      <div>
+        <p class=" text-primary fs-5">6</p>
+        <p class="text-white">Días de la semana</p>
+      </div>
+      <div>
+        <p class=" text-primary fs-5">10</p>
+        <p class="text-white">Hras del Día</p>
+      </div> 
+    </section>
+  </div>
+</div>
+
+<section id="seccion-contacto" class="border-bottom border-secondary border-2">
+  <div id="bg-contacto">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#198754" fill-opacity="1" d="M0,288L40,250.7C80,213,160,139,240,112C320,85,400,107,480,122.7C560,139,640,149,720,138.7C800,128,880,96,960,106.7C1040,117,1120,171,1200,197.3C1280,224,1360,224,1400,224L1440,224L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+</svg>
+  </div>
+
+  <div class="container" id="contenedor-formulario">
+  <div id="titulo-formulario" class="text-center mb-4">
+    <div><img src="./img/contac.png" class="img-fluid" alt=""></div>
+    <h2 class="text-white">Contactanos</h2>
+    <p class="text-white fs-5">Te estamos esperando</p>
+  </div>
+  <form action="">
+  <div class="mb-3">
+    <input type="email" class="form-control" id="text" placeholder="Nombre Completo" require>
+  </div>
+  <div class="mb-3">
+    <input type="tel" class="form-control" id="tel" placeholder="Nombre Completo" require>
+  </div>
+  <div class="mb-3">
+    <input type="email" class="form-control" id="email" placeholder="nombre@ejemplo.com" require>
+  </div>
+  <div class="mb-3">
+    <textarea class="form-control" id="nombre" rows="3" placeholder="Escribe tu mensaje"></textarea>
+  </div>
+  <div class="mb-3">
+    <button type="button" class="btn btn-success w-100 fs-5">Enviar Mensaje</button>
+  </div>
+  </form>
+</div>
+</section>
+<footer class="w-100 d-flex align-items-center justify-content-center">
+  <p class="fs-5 px-3 pt-3">Soluciones Capital Humano. &copy; Todos los Derchos Reservados 2021</p>
+  <div id="iconos">
+    <a href=""><i class="bi bi-facebook"></i></a>
+    <a href="#"><i class="bi bi-youtube"></i></a>
+    <a href="#"><i class="bi bi-twitter"></i></a>
+  </div>
+</footer>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="./libs/bootstrap/js/bootstrap.bundle.min.js" ></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
